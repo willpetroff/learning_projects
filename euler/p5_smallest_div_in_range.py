298e151eeb8b8a581c9cebf7a6608e5e49f4e49b
William Petroff <willpetroff@gmail.com>
2016-06-18 14:30:25 -0400

    style cleanup

--- euler/p5_smallest_div_in_range.py
+++ euler/p5_smallest_div_in_range.py
@@ -1,16 +1,17 @@
 def div_within_range(upper_limit):
-    even_divis=False
-    counter=1
-    while even_divis==False:
-        check_test=0
-        for i in range (1,upper_limit+1):
-            if counter%i==0:
-                check_test+=1
-        if check_test==upper_limit:
-            even_divis=True
+    even_divis = False
+    counter = 1
+    while even_divis == False:
+        check_test = 0
+        for i in range(1, upper_limit + 1):
+            if counter % i == 0:
+                check_test += 1
+        if check_test == upper_limit:
+            even_divis = True
         else:
-            counter+=1
+            counter += 1
     return counter
+
 
 """def div_faster(upper_limit):
     value=1
@@ -26,7 +27,8 @@ def div_within_range(upper_limit):
     value*=((2**value_2)*(3**value_3))
     return value"""
 
+
 def main():
-    user_input=int(input('Please enter a positive int: '))
-    #print (div_within_range(user_input))
-    print (div_faster(user_input))
+    user_input = int(input('Please enter a positive int: '))
+    # print (div_within_range(user_input))
+    print(div_faster(user_input))
